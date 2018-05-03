974d52f10605e70a564259ae0598a76bd497baf4
Carlos Adolfo Ortiz Quiros (Mac) <carlosaortiz@yahoo.com>
2018-05-02 22:03:25 -0500

    Adds Global Constants

---
+++ app-code/call-center/src/main/java/com/csoftz/almundo/call/center/common/consts/GlobalConstants.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   GLOBALCONSTANTS.JAVA                                        */
+/* Description:   General constant definitions.                               */
+/* Author:        Carlos Adolfo Ortiz Quirós (COQ)                            */
+/* Date:          May.02/2018                                                 */
+/* Last Modified: May.02/2018                                                 */
+/* Version:       1.1                                                         */
+/* Copyright (c), 2018 CSoftZ                                                 */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ May.02/2018  COQ  File created.
+ -----------------------------------------------------------------------------*/
+package com.csoftz.almundo.call.center.common.consts;
+
+/**
+ * Employe Service tests
+ *
+ * @author Carlos Adolfo Ortiz Quirós (COQ)
+ * @version 1.1, May.02/2018
+ * @since 1.8 (JDK), May.02/2018
+ */
+public class GlobalConstants {
+    /*
+     * General
+     */
+    public static final String CALL_CENTER_DISPATCHER_NULL_PHONE_NUM_SUPLLIED = "No phone number supplied.";
+    public static final String CALL_CENTER_DISPATCHER_CALL_IN_PROGRESS = "Call is being attended";
+}
